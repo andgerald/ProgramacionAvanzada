2ac04f0f9fdd4faebc435921b4280688a1e00f5d
andgeral <geraldinebecerra93@gmail.com>
2024-03-12 12:01:24 -0300

    fix/se  simplifica  cuando se selecciona un valor el cambio de la imagen

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,24 @@
 import Animal from "./animal.js ";
 import Leon from "./leon.js";
+
+let animalExists;
+let src;
 const selectImage = document.getElementById("preview");
 selectImage.addEventListener("change", () => {});
 
-// select que trae el valor del animal
+// select que trae el valor del animal hago el llamado al json para comprobar de que existe
 const selecAnimal = document.getElementById("animal");
-selecAnimal.addEventListener("change", () => {
-  switch (selecAnimal.value) {
-    case "Leon":
-      selectImage.innerHTML = `
-      <img  style="width:  180px" src='../assets/imgs/Leon.png' alt='León' /> `;
-      break;
-    case "Lobo":
-      selectImage.innerHTML = `
-      <img style="width:  180px" src='../assets/imgs/Lobo.jpg' alt='Lobo' /> `;
-      break;
-    case "Oso":
-      selectImage.innerHTML = `
-      <img style="width:  180px" src='../assets/imgs/Oso.jpg' alt='Oso' /> `;
-      break;
-    case "Serpiente":
-      selectImage.innerHTML = `<img style="width:  180px" src='../assets/imgs/Serpiente.jpg' alt='Serpiente' /> `;
-      break;
-    case "Aguila":
-      selectImage.innerHTML = `<img style="width:  180px" src='../assets/imgs/Aguila.png' alt='Aguila' /> `;
-      break;
-  }
-
-  console.log(selecAnimal.value);
+selecAnimal.addEventListener("change", async () => {
+  const url = "../animales.json";
+  const response = await fetch(url);
+  const datos = await response.json();
+  const animales = datos.animales;
+  const animalValue = selecAnimal.value;
+  animalExists = animales.find((animal) => animal.name === animalValue);
+  const path = animalExists.imagen;
+  src = `../assets/imgs/${path}`;
+  selectImage.innerHTML = `
+     <img  style="width:  180px" src=${src} alt='León' /> `;
 });
 
 // select que trae el valor de la edad
@@ -45,19 +36,13 @@ comentarios.addEventListener("change", () => {
 const registrar = document.getElementById("btnRegistrar");
 registrar.addEventListener("click", async () => {
   const preview = document.getElementById("Animales");
-  const audio = document.getElementById("player");
-  preview.innerHTML += `
-    <img src='../assets/imgs/Lobo.jpg' />
-    <img style="width: 30px" src='../assets/imgs/audio.svg' />
 
-      `;
-  console.log("first");
+  preview.innerHTML += `
+  <img  style="width:  180px" src=${src} alt='León' /> `;
   const leonsito = new Leon(
     selecAnimal.value,
     selecEdad.value,
     selectImage.value,
     comentarios.value
   );
-
-  console.log(leonsito.Rugir(), "SOY EL LEON");
 });
